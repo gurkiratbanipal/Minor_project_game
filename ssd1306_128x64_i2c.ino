5c5653a6ba5e4e387b50c385313483e6803441f7
Gurkirat Singh <55200846+gurkiratbanipal@users.noreply.github.com>
2019-12-15 17:52:45 -0500

    Update ssd1306_128x64_i2c.ino

--- ssd1306_128x64_i2c.ino
+++ ssd1306_128x64_i2c.ino
@@ -313,7 +313,7 @@ void testdrawstyles(void) {
   display.setTextSize(1);             // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE);        // Draw white text
   display.setCursor(0,0);             // Start at top-left corner
-  display.println(F("Hello, world!"));
+  display.println(F("Gurkirat!"));
 
   display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
   display.println(3.141592);
